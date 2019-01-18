f389e1f026ff3d620f325750ddc1a89fdb1020ea
Nabil Fannane <mzabi30@gmail.com>
2019-01-18 10:51:55 -0800

    test

---
+++ test.js
@@ -0,0 +1,23 @@
+
+
+// reerange=(input)=>{
+
+    
+//     let res = input.split('').sort((a, b) => a > b)
+//     let res2=[];
+//     //let res3=[];
+
+//     res.forEach((val,i) => {
+      
+//        if (res2.indexOf(val)==-1) res2.push(val) 
+         
+//       })
+
+//       //console.log(res3);
+//     }
+
+
+//     reerange('aaabc'); 
+
+
+
