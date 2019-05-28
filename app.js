c5701348c2a073eadd231e3fdcac133fc3767032
Nabil Fannane <nabil@goadroit.com>
2019-05-28 12:36:46 -0700

    test

--- app.js
+++ app.js
@@ -203,7 +203,7 @@
 
 
 
-const express = require('express')
+//const express = require('express')
 // // const jwt=require('jsonwebtoken')
 
 // // const app=express();
@@ -271,7 +271,7 @@ const express = require('express')
 // //    const express=require('express')
 // //   //const path=require('path')
 // //   //const cookieParser=require('cookie-parser')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 
 
 // //   //const exphs=require('express-handlebars')
@@ -638,44 +638,44 @@ const bodyParser = require('body-parser')
 
 
 
-let index=[]
-let values=[]
- 
- let tab=[-1,290,340,-1,170,-1,-1,200,380]
-
-   tab.forEach((tab,i)=>{
-    if (tab === -1){
-        index.push(i)
-      }else {
-          values.push(tab)
-      }
-
-   })
-
-  let sort=values.sort()
-
-   index.forEach(tab=>{
-    sort.splice(tab,0,-1)
-   })
- 
- 
-   console.log(sort)
-
-   
-
-  
-
-    
-
-
-
-    
 
 
 
 
 
+scene = new THREE.Scene();
 
+// Init camera (PerspectiveCamera)
+camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
+renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
 
+// Set size (whole window)
+renderer.setSize(window.innerWidth, window.innerHeight);
 
+// Render to canvas element
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(3, 3, 3);
+const material = new THREE.MeshBasicMaterial({
+    color: 0x0000ff
+});
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+camera.position.z = 5;
+
+const animate = () => {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
+}
+animate();
--- welcom.mjs
+++
@@ -1,1 +0,0 @@
-export const welcom ="Welcom from welcom.mj"
